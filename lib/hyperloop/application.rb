577b5e0476edad4faed329d772f70210df948b77
Jake Boxer <jake@github.com>
2013-09-20 20:37:51 -0600

    Clean up app#call a bit
    * Add Application#view_path(request)

--- lib/hyperloop/application.rb
+++ lib/hyperloop/application.rb
@@ -13,20 +13,33 @@ module Hyperloop
     def call(env)
       request  = Rack::Request.new(env)
       response = Response.new
-
-      path = File.join(@views_path, request.path).chomp('/')
-
-      # If we're currently pointing to a directory, get index in it.
-      path = File.join(path, 'index') if Dir.exist?(path)
-      path += '.html'
+      path     = view_path(request)
 
       if File.exist?(path)
-        response.write(File.read(path))
+        # If there's a file at the view path, use its data as the response body.
+        data = File.read(path)
+        response.write(data)
       else
+        # If there's no file at the view path, 404.
         response.status = 404
       end
 
       response.finish
+    end
+
+    private
+
+    # Internal: Get the view path for the specified request.
+    #
+    # request - Rack::Request to get the view path for.
+    #
+    # Returns a String.
+    def view_path(request)
+      path = File.join(@views_path, request.path).chomp('/')
+
+      # If we're currently pointing to a directory, get index in it.
+      path = File.join(path, 'index') if Dir.exist?(path)
+      path + '.html'
     end
   end
 end
