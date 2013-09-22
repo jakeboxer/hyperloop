5ee7a0f54f66cda85ada921024ae400c19a77af2
Jake Boxer <jake@github.com>
2013-09-22 12:33:48 -0600

    Refactor View spec
    * Move view creation into before block

--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -1,39 +1,38 @@
 require 'hyperloop'
 
 describe Hyperloop::View do
+  before :each do
+    @html_view = Hyperloop::View.new('spec/fixtures/simple/app/views/about.html')
+    @erb_view  = Hyperloop::View.new('spec/fixtures/erb/app/views/about.html.erb')
+  end
+
   describe '#format' do
     it 'is :html for HTML files' do
-      view = Hyperloop::View.new('spec/fixtures/simple/app/views/about.html')
-      expect(view.format).to eql(:html)
+      expect(@html_view.format).to eql(:html)
     end
 
     it 'is :erb for ERB files' do
-      view = Hyperloop::View.new('spec/fixtures/erb/app/views/about.html.erb')
-      expect(view.format).to eql(:erb)
+      expect(@erb_view.format).to eql(:erb)
     end
   end
 
   describe '#name' do
     it 'strips the extension from plain HTML files' do
-      view = Hyperloop::View.new('spec/fixtures/simple/app/views/about.html')
-      expect(view.name).to eql('about')
+      expect(@html_view.name).to eql('about')
     end
 
     it 'strips the extension from ERB files' do
-      view = Hyperloop::View.new('spec/fixtures/erb/app/views/about.html.erb')
-      expect(view.name).to eql('about')
+      expect(@erb_view.name).to eql('about')
     end
   end
 
   describe '#render' do
     it 'renders plain HTML files' do
-      view = Hyperloop::View.new('spec/fixtures/simple/app/views/index.html')
-      expect(view.render).to match(/<h1>Simple/)
+      expect(@html_view.render).to match(/<h1>About/)
     end
 
     it 'renders ERB files' do
-      view = Hyperloop::View.new('spec/fixtures/erb/app/views/index.html.erb')
-      expect(view.render).to match(/<h1>WE ARE USING ERB/)
+      expect(@erb_view.render).to match(/<h1>I was born on December 21/)
     end
   end
 end
