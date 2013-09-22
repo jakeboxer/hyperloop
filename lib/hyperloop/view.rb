1ce7f6eb49927306308bef80d39e54d070788b94
Jake Boxer <jake@github.com>
2013-09-22 12:05:53 -0600

    Make View#render handle ERB files

--- lib/hyperloop/view.rb
+++ lib/hyperloop/view.rb
@@ -1,12 +1,20 @@
+require 'erb'
+
 module Hyperloop
   class View
     def initialize(full_path)
       @full_path = full_path
       @data      = File.read(@full_path)
+      @format    = File.extname(@full_path)[1..-1]
     end
 
     def render
-      @data
+      case @format
+      when 'html'
+        @data
+      when 'erb'
+        ERB.new(@data).result
+      end
     end
   end
 end
--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -1,8 +1,15 @@
 require 'hyperloop'
 
 describe Hyperloop::View do
-  it 'renders plain html files' do
-    view = Hyperloop::View.new('spec/fixtures/simple/app/views/index.html')
-    expect(view.render).to match(/<h1>Simple/)
+  describe '#render' do
+    it 'renders plain HTML files' do
+      view = Hyperloop::View.new('spec/fixtures/simple/app/views/index.html')
+      expect(view.render).to match(/<h1>Simple/)
+    end
+
+    it 'renders ERB files' do
+      view = Hyperloop::View.new('spec/fixtures/erb/app/views/index.html.erb')
+      expect(view.render).to match(/<h1>WE ARE USING ERB/)
+    end
   end
 end
