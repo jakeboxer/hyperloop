cb2f15af7867b2915d32b4439e1b108c4536735f
Jake Boxer <jake@github.com>
2013-10-19 17:16:01 -0500

    Move view lookup logic from app to Registry

--- lib/hyperloop/application.rb
+++ lib/hyperloop/application.rb
@@ -6,38 +6,8 @@ module Hyperloop
     include Rack::Utils
 
     def initialize(root=nil)
-      @root       = root
-      @views_root = File.join([@root, "app/views"].compact)
-      layout_path = @views_root + "/layouts/application.html.erb"
-
-      # Get all the view paths. These look like:
-      #
-      # some/path/app/views/whatever.html.erb
-      # some/path/app/views/subdir/whatever.html.erb
-      view_paths = Dir.glob(@views_root + "/**/*").reject {|fn| File.directory?(fn)}
-
-      @views = view_paths.inject({}) do |result, path|
-        view = View.new(path, layout_path)
-
-        # The path under app/views. This will be something like:
-        #
-        # /whatever.html.erb
-        # /subdir/whatever.html.erb
-        relative_path = path.sub(@views_root, "")
-
-        # The path under app/views without a file extension. This will be
-        # something like:
-        #
-        # /whatever
-        # /subdir/whatever
-        request_dir  = File.split(relative_path).first
-        request_path = File.join(request_dir, view.name)
-
-        result[request_path] = view
-        result[request_dir]  = view if view.name == "index"
-
-        result
-      end
+      @root          = root
+      @view_registry = View::Registry.new(@root)
     end
 
     # Rack call interface.
@@ -52,7 +22,7 @@ module Hyperloop
         # as the response body.
         response["Content-Type"] = asset.content_type
         response.write(asset.source)
-      elsif view = @views[request_path]
+      elsif view = @view_registry.find_template_view(request_path)
         # If there's a view at the path, use its data as the response body.
         data = view.render(request)
         response.write(data)
--- lib/hyperloop/view/registry.rb
+++ lib/hyperloop/view/registry.rb
@@ -2,6 +2,42 @@ module Hyperloop
   class View
     class Registry
       def initialize(root)
+        @root       = root
+        @views_root = File.join([@root, "app/views"].compact)
+        layout_path = @views_root + "/layouts/application.html.erb"
+
+        # Get all the view paths. These look like:
+        #
+        # some/path/app/views/whatever.html.erb
+        # some/path/app/views/subdir/whatever.html.erb
+        view_paths = Dir.glob(@views_root + "/**/*").reject {|fn| File.directory?(fn)}
+
+        @views = view_paths.inject({}) do |result, path|
+          view = View.new(path, layout_path)
+
+          # The path under app/views. This will be something like:
+          #
+          # /whatever.html.erb
+          # /subdir/whatever.html.erb
+          relative_path = path.sub(@views_root, "")
+
+          # The path under app/views without a file extension. This will be
+          # something like:
+          #
+          # /whatever
+          # /subdir/whatever
+          request_dir  = File.split(relative_path).first
+          request_path = File.join(request_dir, view.name)
+
+          result[request_path] = view
+          result[request_dir]  = view if view.name == "index"
+
+          result
+        end
+      end
+
+      def find_template_view(path)
+        @views[path]
       end
     end
   end
