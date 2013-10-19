463f93e7773159570266c442951a25d0114b8805
Jake Boxer <jake@github.com>
2013-10-19 17:23:25 -0500

    Split up registry store into templates + partials

--- lib/hyperloop/view/registry.rb
+++ lib/hyperloop/view/registry.rb
@@ -10,34 +10,42 @@ module Hyperloop
         #
         # some/path/app/views/whatever.html.erb
         # some/path/app/views/subdir/whatever.html.erb
+        # some/path/app/views/subdir/_partial.html.erb
         view_paths = Dir.glob(@views_root + "/**/*").reject {|fn| File.directory?(fn)}
 
-        @views = view_paths.inject({}) do |result, path|
+        @template_views = {}
+        @partial_views  = {}
+
+        view_paths.each do |path|
           view = View.new(path, layout_path)
 
           # The path under app/views. This will be something like:
           #
           # /whatever.html.erb
           # /subdir/whatever.html.erb
+          # /subdir/_partial.html.erb
           relative_path = path.sub(@views_root, "")
 
-          # The path under app/views without a file extension. This will be
-          # something like:
+          # The path under app/views without a file extension (and without the
+          # starting _ for partials). This will be something like:
           #
           # /whatever
           # /subdir/whatever
+          # /subdir/partial
           request_dir  = File.split(relative_path).first
           request_path = File.join(request_dir, view.name)
 
-          result[request_path] = view
-          result[request_dir]  = view if view.name == "index"
-
-          result
+          if view.partial?
+            @partial_views[request_path] = view
+          else
+            @template_views[request_path] = view
+            @template_views[request_dir]  = view if view.name == "index"
+          end
         end
       end
 
       def find_template_view(path)
-        @views[path]
+        @template_views[path]
       end
     end
   end
