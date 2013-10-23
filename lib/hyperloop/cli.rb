0ed8341ba57884ca8ae405b61d2dc238ce088c92
Jake Boxer <jake@github.com>
2013-10-22 22:04:16 -0700

    Add stub `hyperloop new SITENAME` command

--- lib/hyperloop/cli.rb
+++ lib/hyperloop/cli.rb
@@ -2,5 +2,9 @@ require "thor"
 
 module Hyperloop
   class CLI < Thor
+    desc "new SITENAME", "Create a new Hyperloop site. \"rails new my_site\" creates a new site called MySite in \"./my_site\""
+    def new(name)
+      puts "you tried to make a site named #{name}! this isn't implemented yet! lol!"
+    end
   end
 end
