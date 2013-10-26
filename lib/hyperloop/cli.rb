689b395672f39927bde2b4a61654a3f6b6f7e500
Jake Boxer <jake@github.com>
2013-10-25 19:46:41 -0700

    Generate real app template

--- lib/hyperloop/cli.rb
+++ lib/hyperloop/cli.rb
@@ -5,7 +5,7 @@ module Hyperloop
   class CLI < Thor
     desc "new SITENAME", "Create a new Hyperloop site. \"rails new my_site\" creates a new site called MySite in \"./my_site\""
     def new(name)
-      Hyperloop::Generators::Site.start(["site", name])
+      Hyperloop::Generators::Site.start([name])
     end
   end
 end
--- lib/hyperloop/generators/site.rb
+++ lib/hyperloop/generators/site.rb
@@ -1,23 +1,31 @@
-require 'thor/group'
+require "thor/group"
+require "hyperloop/version"
 
 module Hyperloop
   module Generators
     class Site < Thor::Group
       include Thor::Actions
 
-      argument :group, :type => :string
       argument :name, :type => :string
 
       def self.source_root
         File.join(File.dirname(__FILE__), "site")
       end
 
-      def create_group
-        empty_directory(group)
+      def create_directories
+        empty_directory(name)
+        empty_directory(File.join(name, "app"))
+        empty_directory(File.join(name, "app", "views"))
+        empty_directory(File.join(name, "app", "views", "layouts"))
       end
 
-      def copy_root
-        template("index.html.erb", "#{group}/#{name}/index.html.erb")
+      def copy_files
+        template("config.ru",            File.join(name, "config.ru"))
+        template("Gemfile",              File.join(name, "Gemfile"))
+        template("application.html.erb", File.join(name, "app", "views", "layouts", "application.html.erb"))
+        template("index.html.erb",       File.join(name, "app", "views", "index.html.erb"))
+        template("about.html.erb",       File.join(name, "app", "views", "about.html.erb"))
+        template("_partial.html.erb",    File.join(name, "app", "views", "_partial.html.erb"))
       end
     end
   end
