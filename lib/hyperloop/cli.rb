fd31ae292503477497ea17060134c5e9063d6b70
Jake Boxer <jake@github.com>
2013-10-26 13:00:33 -0700

    Fix `hyperloop new` description

--- lib/hyperloop/cli.rb
+++ lib/hyperloop/cli.rb
@@ -3,7 +3,7 @@ require "thor"
 
 module Hyperloop
   class CLI < Thor
-    desc "new SITENAME", "Create a new Hyperloop site. \"rails new my_site\" creates a new site called MySite in \"./my_site\""
+    desc "new SITENAME", "Create a new Hyperloop site. \"hyperloop new my_site\" creates a new site called MySite in \"./my_site\""
     def new(name)
       Hyperloop::Generators::Site.start([name])
     end
